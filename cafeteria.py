c4082d44ca9a9db5419fc16d9e58fa74c966b6fd
Admin <admin@Intern5-MacBook-Pro.local>
2019-03-07 16:00:11 +0530

    Order cold and hot beverages

--- cafeteria.py
+++ cafeteria.py
@@ -17,7 +17,7 @@ def employee_page():
     return render_template('employee_page.html')
 
 
-@app.route('/beverages', methods=['POST'])
+@app.route('/employee-choice', methods=['POST'])
 def post_data_request():
     return validate_data(connection, request.form)
 
@@ -25,37 +25,29 @@ def post_data_request():
 def validate_data(connection, user_data):
     cursor = connection.cursor()
     cursor.execute("select employee_id from employee_details where employee_id=%(id)s",
-                   {'id': user_data['employee_id']})
+                   {'id': user_data['employee_id']}, )
     returned_rows = cursor.fetchall()
     cursor.close()
     if len(returned_rows) == 0:
         return render_template('welcome_page.html')
     else:
-        post_data(connection, user_data)
-        return render_template('types_of_beverages_employee.html')
+        return render_template('types_of_beverages_employee.html', item=user_data['employee_id'])
 
 
-def post_data(connection, user_data):
-    cursor = connection.cursor()
-    cursor.execute("""insert into login_table(employee_id,username) values( %s, %s);""",
-                   (user_data['employee_id'], user_data['username']), )
-    connection.commit()
-    cursor.close()
-    return cursor.execute
+@app.route('/beverages-for-cold', methods=['POST'])
+def cold_all_items():
+    return cold_item(connection, request.form)
 
 
-@app.route('/employee-choice')
-def employee_choice():
-    return render_template('types_of_beverages_employee.html')
-
-
-@app.route('/beverages-for-cold')
-def cold_item():
+def cold_item(connection, user_data):
+    connection.cursor()
+    array_values = tuple(user_data.values())
+    array_value = array_values[0]
     rows = display_available_cold_items()
     items = []
     for row in rows:
         items.append(row[0])
-    return render_template("list_cold_beverages_jinja.html", items=items)
+    return render_template("list_cold_beverages_jinja.html", items=items, employee_id=array_value)
 
 
 def display_available_cold_items():
@@ -65,21 +57,93 @@ def display_available_cold_items():
     return record
 
 
-@app.route('/beverages-for-hot')
-def hot_items():
-    rows = database_selected_hot_items()
+@app.route('/beverages-for-hot', methods=['POST'])
+def hot_all_items():
+    return hot_item(connection, request.form)
+
+
+def hot_item(connection, user_data):
+    connection.cursor()
+    array_values = tuple(user_data.values())
+    array_value = array_values[0]
+    rows = display_available_hot_items()
     items = []
     for row in rows:
         items.append(row[0])
-    return render_template("list_hot_beverages_jinja.html", items=items)
+    return render_template("list_hot_beverages_jinja.html", items=items, employee_id=array_value)
 
 
-def database_selected_hot_items():
+def display_available_hot_items():
     cursor = connection.cursor()
     cursor.execute("select name from beverage_details where is_available  ='yes' AND vendor_id=1002")
     record = cursor.fetchall()
 
     return record
+
+
+@app.route('/cold-item', methods=['POST'])
+def update_item():
+    return update(connection, request.form)
+
+
+def update(connection, update_data):
+    item = update_data.to_dict()
+    a = []
+    b = []
+    c = []
+    i = 1
+    j = 2
+    index = 0
+    if i != len(item):
+        for row in range(len(item)):
+            a.append(list(item.keys())[i])
+            b.append(list(item.values())[j])
+            c.append(list(item.values())[0])
+            cursor = connection.cursor()
+            update_details = "insert into ordered_items (item_id,quantity,employee_id) select item_id,{},{} from beverage_details where name = '{}'".format(
+                b[index], c[0], a[index])
+            cursor.execute(update_details)
+            connection.commit()
+            cursor.close()
+            i += 2
+            j += 2
+            index += 1
+    return update_details
+
+
+@app.route('/hot-item', methods=['POST'])
+def update_item1():
+    return update1(connection, request.form)
+
+
+def update1(connection, update_data):
+    item = update_data.to_dict()
+    a = []
+    b = []
+    c = []
+    i = 1
+    j = 2
+    index = 0
+    if i != len(item):
+        for row in range(len(item)):
+            a.append(list(item.keys())[i])
+            b.append(list(item.values())[j])
+            c.append(list(item.values())[0])
+            cursor = connection.cursor()
+            update_details1 = "insert into ordered_items (item_id,quantity,employee_id) select item_id,{},{} from beverage_details where name = '{}'".format(
+                b[index], c[0], a[index])
+            cursor.execute(update_details1)
+            connection.commit()
+            cursor.close()
+            i += 2
+            j += 2
+            index += 1
+    return update_details1
+
+
+# @app.route('/final-page')
+# def final_page():
+#     return render_template('final_page.html')
 
 
 @app.route('/vendor-login')
